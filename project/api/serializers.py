f623f744d3c34fd11b539f7f1b0a0cda9aadf096
Pragati Gupta <pragatigupta0104@gmail.com>
2025-02-18 15:34:47 +0530

    p

--- project/api/serializers.py
+++ project/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .models import Student
 
 class Stu_serializers(serializers.Serializer):
 
@@ -7,3 +8,17 @@ class Stu_serializers(serializers.Serializer):
     stu_email=serializers.EmailField()
     stu_contat=serializers.IntegerField()
     stu_city=serializers.CharField(max_length=50)
+
+    def create(self, validated_data):
+        
+        return Student.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+       
+        instance.stu_name = validated_data.get('stu_name', instance.stu_name)
+        instance.stu_email = validated_data.get('stu_email', instance.stu_email)
+        instance.stu_contact = validated_data.get('stu_contact', instance.stu_contact)
+        instance.stu_city = validated_data.get('stu_city', instance.stu_city)
+       
+        instance.save()
+        return instance
--- project/api/views.py
+++ project/api/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+'''from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from .models import Student
 from .serializers import Stu_serializers
-from rest_framework.renderers import JSONRenderer
+from rest_framework.renderers import JSONRenderer'''
 #from rest_framework.parsers import JSONParser
 #import io
 
 # Create your views here.
-def stu_list(request):
+'''def stu_list(request):
     stu = Student.objects.all()
     print(stu)
     serializer =Stu_serializers(stu,many=True)
@@ -21,4 +21,61 @@ def stu_detail(request,pk):
     serializer = Stu_serializers(user)
     print("Serializer=",serializer)
     print(serializer.data)
-    return JsonResponse(serializer.data,safe=False)
+    return JsonResponse(serializer.data,safe=False)'''
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import Student
+from .serializers import Stu_serializers
+
+@csrf_exempt
+def stu_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Student.objects.all()
+        serializer = Stu_serializers(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = Stu_serializers(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+   
+@csrf_exempt
+def stu_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = Stu_serializers(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = Stu_serializers(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'PATCH':
+        data = JSONParser().parse(request)
+        serializer = Stu_serializers(snippet, data=data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        stu.delete()
+        return HttpResponse(status=204)
