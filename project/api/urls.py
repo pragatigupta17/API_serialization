4006e18b18261f03e707988d72ec941ec9b1f2a7
Pragati Gupta <pragatigupta0104@gmail.com>
2025-02-24 21:03:37 +0530

    gj

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .import views
 from .views import Stu_List
+from .views import Stu_Detail
 
 urlpatterns =[
   #  path('stu_list/',views.stu_list),
   # path('stu_detail/<int:pk>/',views.stu_detail),
-    path('stu_list/',Stu_List.as_view(),name='stu_list')
+    path('stu_list/',Stu_List.as_view(),name='stu_list'),
+    path('stu_detail/<int:pk>/',Stu_Detail.as_view(),name='stu_detail')
 
   
 
